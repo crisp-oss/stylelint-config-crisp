904515807f0348143675381dc83ae591306d3f0f
Eliott Vincent <dev@eliottvincent.com>
2024-01-01 17:59:48 +0100

    v1.0.20
    Improve class checker

--- rules/selector-class-interpolation.js
+++ rules/selector-class-interpolation.js
@@ -10,7 +10,7 @@ const ruleFunction = (primaryOption, secondaryOptions = {}, context) => {
     // Validate options for the rule
     const validOptions = stylelint.utils.validateOptions(result, ruleName, {
       actual: primaryOption,
-      possible: [true, false]
+      possible: [true]
     }, {
       actual: secondaryOptions,
       possible: {
@@ -23,60 +23,73 @@ const ruleFunction = (primaryOption, secondaryOptions = {}, context) => {
       return;
     }
 
-    root.walkRules(rule => {
-      // Skip non-Vue files
-      if (!root.source.input.file.endsWith(".vue")) {
+    // Skip non-Vue files
+    if (!root.source.input.file.endsWith(".vue")) {
+      return;
+    }
+
+    let fileScopeClass;
+
+    // First pass (identify the parent selector - via variable)
+    root.walkDecls(decl => {
+      if (decl.prop === "$c") {
+        // Remove leading `.` and `"`
+        fileScopeClass = decl.value.replace(/^["']?\./, "").replace(/["']?$/, "");
+
         return;
       }
+    });
 
-      // Skip rules without selectors (e.g., @font-face, @keyframes)
+    if (!fileScopeClass) {
+      // Second pass (identify the parent selector - via first class)
+      root.walkRules(rule => {
+        if (rule.parent && rule.parent.type === "root") {
+          const fileScopeClassMatch = rule.selector.match(/^\.[\w-]+/);
+
+          if (fileScopeClassMatch) {
+            // Capture the class name without the dot
+            fileScopeClass = fileScopeClassMatch[0].substring(1);
+
+            return;
+          }
+        }
+      });
+    }
+
+    root.walkRules(rule => {
+      // Skip rules without selectors (e.g. @font-face, @keyframes, variables)
       if (!rule.selector) {
         return;
       }
 
-      // Resolve the actual selector name
-      const resolvedSelector = resolveSelector(rule);
-      const ignoreSelectors = secondaryOptions.ignoreSelectors || [];
-
-      // Determine if the resolved selector should be ignored
-      const isIgnored = ignoreSelectors.some(ignoreItem => {
-        if (typeof ignoreItem === "string") {
-          // Check if it's a stringified regex (e.g., "/pattern/")
-          if (ignoreItem.startsWith("/") && ignoreItem.endsWith("/")) {
-            const regexPattern = ignoreItem.slice(1, -1);
-            const regex = new RegExp(regexPattern);
-            return regex.test(resolvedSelector);
-          }
-
-          // It's a regular string
-          return ignoreItem === resolvedSelector;
-        }
-
-        // It's a real RegExp
-        return ignoreItem.test(resolvedSelector);
-      });
-
-      if (isIgnored) {
+      // Skip selector if ignored via rule options
+      if (isSelectorIgnored(rule, secondaryOptions.ignoreSelectors || [])) {
         return;
       }
 
-      if (context.fix) {
-        let newSelector = rule.selector;
+      // Skip selectors other than `.*`, `&__*` , `&-*` or `&.*`
+      if (!rule.selector.startsWith(".") &&
+          !rule.selector.startsWith("&__") &&
+          !(rule.selector.startsWith("&-") && !rule.selector.startsWith("&--")) &&
+          !rule.selector.startsWith("&.")
+      ) {
+        return;
+      }
 
-        // Auto-fix for top-level class selector
-        if (isTopLevelClassSelector(rule)) {
-          const className = rule.selector.substring(1);
-          newSelector = `#{$c}`;
-          rule.root().prepend(`$c: ".${className}";\n`);
-        } else {
-          // Auto-fix logic for nested selectors
-          newSelector = applyNestedSelectorFixes(rule, newSelector);
+      let isInterpolated = isInterpolatedClassSelector(rule);
+
+      if (!isInterpolated) {
+        // Skip classes elements outside of CSS file scope
+        if (rule.selector.startsWith(".") &&
+            !rule.selector.startsWith(`.${fileScopeClass}`)) {
+          return;
         }
 
-        rule.selector = newSelector;
-      } else {
-        // Reporting logic for selectors not using Sass interpolation
-        reportNonInterpolatedSelectors(rule, result, primaryOption);
+        if (context.fix) {
+          fixSelector(rule);
+        } else {
+          reportSelector(rule, result);
+        }
       }
     });
   };
@@ -84,7 +97,7 @@ const ruleFunction = (primaryOption, secondaryOptions = {}, context) => {
 
 // Helper function to resolve the actual selector name
 function resolveSelector(rule, parentSelector = "") {
-  // Skip rules without selectors (e.g., @font-face, @keyframes)
+  // Skip rules without selectors (e.g. @font-face, @keyframes)
   if (!rule.selector) {
     return "";
   }
@@ -92,7 +105,12 @@ function resolveSelector(rule, parentSelector = "") {
   if (rule.selector.startsWith("&")) {
     const combinedSelector = (parentSelector + " " + rule.selector.replace("&", "")).trim();
 
-    return rule.parent && rule.parent.type !== "root" ? resolveSelector(rule.parent, combinedSelector) : combinedSelector;
+    // Recurse?
+    if (rule.parent && rule.parent.type !== "root") {
+      return resolveSelector(rule.parent, combinedSelector);
+    }
+
+    return combinedSelector;
   }
 
   return parentSelector ? (parentSelector + " " + rule.selector).trim() : rule.selector;
@@ -103,37 +121,99 @@ function isStringOrRegExp(value) {
   return value instanceof RegExp || typeof value === "string";
 }
 
+// Check if selector is ignored via options
+function isSelectorIgnored(rule, ignoreSelectors = []) {
+  // Resolve the actual selector name
+  const resolvedSelector = resolveSelector(rule);
+
+  const isIgnored = ignoreSelectors.some(ignoreItem => {
+    if (typeof ignoreItem === "string") {
+      // Check if it's a stringified regex (e.g. "/pattern/")
+      if (ignoreItem.startsWith("/") && ignoreItem.endsWith("/")) {
+        const regexPattern = ignoreItem.slice(1, -1);
+        const regex = new RegExp(regexPattern);
+
+        return regex.test(resolvedSelector);
+      }
+
+      // It's a regular string
+      return ignoreItem === resolvedSelector;
+    }
+
+    // It's a real RegExp
+    return ignoreItem.test(resolvedSelector);
+  });
+
+  return isIgnored;
+}
+
 // Check if the rule is a top-level class selector
 function isTopLevelClassSelector(rule) {
   return rule.parent && rule.parent.type === "root" && rule.selector.startsWith(".");
 }
 
-// Apply fixes for nested selectors
-function applyNestedSelectorFixes(rule, selector) {
-  let newSelector = selector.replace(/&__([^ ,{]+)/g, "#{$c}__$1");
-  const parentMatch = rule.parent && rule.parent.selector ? rule.parent.selector.match(/__([^ ,{]+)/) : null;
-  if (parentMatch) {
-    newSelector = newSelector.replace(/&-([^ ,{]+)/g, `#{$c}__${parentMatch[1]}-$1`);
+// Check if selector uses Sass interpolation
+function isInterpolatedClassSelector(rule) {
+  return /#\{[^}]+\}/.test(rule.selector);
+}
+
+// Apply fixes for nested selector
+function fixNestedSelector(rule, selector) {
+  let newSelector = selector;
+
+  if (selector.startsWith("&__")) {
+    // Transform `&__foo` to `#{$c}__foo`
+    selector.replace(/&__([^ ,{]+)/g, "#{$c}__$1");
   }
+
+  if (selector.startsWith("&-")) {
+    const parentMatch = (rule.parent && rule.parent.selector)
+      ? rule.parent.selector.match(/__([^ ,{]+)/)
+      : null;
+
+    if (parentMatch) {
+      // Transform
+      // ```
+      // &__foo {
+      //   &-bar {
+      // ```
+      // to
+      // ```
+      // #{$c}__foo {
+      //   #{$c}__foo-bar {
+      // ```
+      newSelector = newSelector.replace(/&-([^ ,{]+)/g, `#{$c}__${parentMatch[1]}-$1`);
+    }
+  }
+
   return newSelector;
 }
 
-// Report selectors that do not use Sass interpolation
-function reportNonInterpolatedSelectors(rule, result, primaryOption) {
-  if (rule.selector.startsWith(".") || rule.selector.startsWith("&__") || rule.selector.startsWith("&.")) {
-    const containsInterpolation = /#\{[^}]+\}/.test(rule.selector);
-    const expected = primaryOption === true;
+// Fix selector
+function fixSelector(rule) {
+  let newSelector = rule.selector;
 
-    if (containsInterpolation !== expected) {
-      stylelint.utils.report({
-        ruleName,
-        result,
-        node: rule,
-        message: messages.expected,
-        word: rule.selector
-      });
-    }
+  if (isTopLevelClassSelector(rule)) {
+    newSelector = `#{$c}`;
+
+    rule.root().prepend(`$c: "${rule.selector}";\n`);
+  } else {
+    // Auto-fix nested selector
+    newSelector = fixNestedSelector(rule, newSelector);
   }
+
+  rule.selector = newSelector;
+}
+
+// Report selectors
+function reportSelector(rule, result) {
+  stylelint.utils.report({
+    ruleName,
+    result,
+    node: rule,
+    message: messages.expected,
+    word: rule.selector
+  });
 }
 
 module.exports = stylelint.createPlugin(ruleName, ruleFunction);
